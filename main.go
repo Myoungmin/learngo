17d275db79b1e567ebe719d33c88d1db408f2216
AzureAD\고명민 <miung222@gmail.com>
2023-08-08 22:37:37 +0900

    #1.2 Variables and Constants

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	//const name string = "nico"
+
+	//var name string = "nico"
+	// 아래는 위와 동일한 코드이다.
+	// Go가 type을 찾아준다.
+	name := "nico"
+
+	name = "lynn"
+	fmt.Println(name)
 }
