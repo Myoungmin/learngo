12da968683c7156810914e982ab442fed439f948
AzureAD\고명민 <miung222@gmail.com>
2023-08-22 21:58:00 +0900

    multiple value를 반환하는 function

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// func multiply(a int, b int) int {
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
 
 func main() {
-	//const name string = "nico"
-
-	//var name string = "nico"
-	// 아래는 위와 동일한 코드이다.
-	// Go가 type을 찾아준다.
-	name := "nico"
-
-	name = "lynn"
-	fmt.Println(name)
+	//fmt.Println(multiply(2, 2))
+	totalLength, upperName := lenAndUpper("Myoungmin")
+	fmt.Println(totalLength, upperName)
 }
