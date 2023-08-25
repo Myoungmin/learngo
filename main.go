fdb01752cd6f4a4e2e51dae51ac4d1503185de2d
AzureAD\고명민 <miung222@gmail.com>
2023-08-25 23:56:08 +0900

    range 사용하여 for문 돌기

--- main.go
+++ main.go
@@ -2,41 +2,15 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-// func multiply(a int, b int) int {
-func multiply(a, b int) int {
-	return a * b
-}
-
-// func lenAndUpper(name string) (int, string) {
-// 	return len(name), strings.ToUpper(name)
-// }
-
-func lenAndUpper(name string) (length int, uppercase string) {
-	// function이 값을 return하고 나면 실행
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func superAdd(numbers ...int) int {
+	for number := range numbers {
+		fmt.Println(number)
+	}
+	return 1
 }
 
 func main() {
-	//fmt.Println(multiply(2, 2))
-
-	// totalLength, upperName := lenAndUpper("Myoungmin")
-	// fmt.Println(totalLength, upperName)
-
-	// totalLength, _ := lenAndUpper("Myoungmin")
-	// fmt.Println(totalLength)
-
-	//repeatMe("nico", "lynn", "dal", "marl", "flynn")
-
-	totalLength, up := lenAndUpper("Myoungmin")
-	fmt.Println(totalLength, up)
+	superAdd(1, 2, 3, 4, 5, 6)
 }
