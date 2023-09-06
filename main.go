bb032e811e1a8571468f4290174cf651bdac2aa4
AzureAD\고명민 <miung222@gmail.com>
2023-09-06 22:56:12 +0900

    조건을 체크하기 전에 variable을 만들 수 있다

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 )
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
+func canIDrink(age int) bool {
+	// 조건을 체크하기 전에 variable을 만들 수 있다
+	if koreanAge := age + 2; koreanAge < 20 {
+		return false
 	}
-	return total
+	return true
 }
 
 func main() {
-	result := superAdd(1, 2, 3, 4, 5, 6)
-	fmt.Println(result)
+	fmt.Println(canIDrink(16))
 }
