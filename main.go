66f6c27ff5a9a17464eea1b89d740cccbcf872f3
Myoungmin <miung222@gmail.com>
2023-09-10 18:23:54 +0900

    append를 이용해 슬라이스에 요소 추가
    반환값을 대입해야 변경된다

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func canIDrink(age int) bool {
-	switch {
-	case age < 19:
-		return false
-	case age >= 19:
-		return true
-	}
-	return false
-}
 func main() {
-	fmt.Println(canIDrink(16))
+	names := []string{"nico", "lynn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
