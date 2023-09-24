cc2b883c941ffcfd0f375fe89d531c60b56f5c35
AzureAD\고명민 <miung222@gmail.com>
2023-09-24 23:25:13 +0900

    golang map의 key를 호출하여 Dictionary  Search 메서드를 정의

--- main.go
+++ main.go
@@ -26,7 +26,19 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{}
-	dictionary["hello"] = "hello"
-	fmt.Println(dictionary)
+	dictionary := mydict.Dictionary{"first": "First word"}
+
+	definition, err := dictionary.Search("second")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
+
+	definition, err = dictionary.Search("first")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 }
