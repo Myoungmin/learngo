73a5a316e1dcf5d75b311eea0a8c1399043dc258
AzureAD\고명민 <miung222@gmail.com>
2023-09-29 14:09:31 +0900

    웹사이트로 접속(hit)하고 그 결과를 알려주는 함수 hitURL 생성, 사용
    hit: 인터넷 웹 서버의 파일 1개에 접속하는 것을 뜻한다.

--- main.go
+++ main.go
@@ -17,25 +17,61 @@
 // 	fmt.Println(account)
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+
+// 	"github.com/Myoungmin/learngo/mydict"
+// )
+
+// func main() {
+// 	dictionary := mydict.Dictionary{}
+// 	baseWord := "hello"
+// 	dictionary.Add(baseWord, "First")
+// 	dictionary.Search(baseWord)
+// 	dictionary.Delete(baseWord)
+// 	word, err := dictionary.Search(baseWord)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		fmt.Println(word)
+// 	}
+
+// }
+
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/Myoungmin/learngo/mydict"
+	"net/http"
 )
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(word)
-	}
+var errRequestFailed = errors.New("Request failed")
 
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
